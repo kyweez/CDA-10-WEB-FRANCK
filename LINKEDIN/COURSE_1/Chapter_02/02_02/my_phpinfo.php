b72d9dea1706cca162e21b5be9e2a06821b36507
kyweez <auboudier.pro@gmail.com>
2020-09-25 08:58:04 +0200

    2.2 OK

--- LINKEDIN/COURSE_1/Chapter_02/02_02/my_phpinfo.php
+++ LINKEDIN/COURSE_1/Chapter_02/02_02/my_phpinfo.php
@@ -1,5 +1,26 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-  phpinfo();
-  
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello World";
+    echo "Hello World";
+    ?>
+    <br>
+    <?php
+    echo "Hello World";
+    ?>
+    <br>
+    <?php
+    echo 2 + 3;
+    ?>
+
+</body>
+
+</html>
