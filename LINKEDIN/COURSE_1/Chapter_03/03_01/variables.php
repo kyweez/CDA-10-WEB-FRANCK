3ba2ac9151c790fc9a6f0b791fbbefb4f42f9e92
kyweez <auboudier.pro@gmail.com>
2020-09-25 09:24:57 +0200

    3.1 OK

---
+++ LINKEDIN/COURSE_1/Chapter_03/03_01/variables.php
@@ -0,0 +1,31 @@
+<html lang="en">
+  <head>
+    <title>Variables</title>
+  </head>
+  <body>
+    <?php
+      $var1 = 10;
+      echo $var1;
+      echo "<br/>";
+
+      $var2 = 100;
+      echo $var2;
+      echo "<br/>";
+
+      $var1 = 200;
+      echo $var1;
+      echo "<br/>";
+
+      echo $var1 + $var2;
+      echo "<br/>";
+
+      echo $var1.$var2;
+      echo "<br/>";
+
+      $var1 = "Coucou ";
+      $var2 = "c'est nous !";
+      echo $var1.$var2;
+      echo "<br/>";
+    ?>
+  </body>
+</html>
