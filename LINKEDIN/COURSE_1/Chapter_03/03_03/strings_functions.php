358728b5bdb6c81d21094c9bc0060ed8e3db9ff4
kyweez <auboudier.pro@gmail.com>
2020-09-28 19:47:16 +0200

    Push globa

--- LINKEDIN/COURSE_1/Chapter_03/03_03/strings_functions.php
+++ LINKEDIN/COURSE_1/Chapter_03/03_03/strings_functions.php
@@ -10,9 +10,17 @@
   $first = "The quick brown fox";
   $second = " jumped over the lazy dog !";
 
-  
+  $third = $first;
+  $third .= $second;
 
+  echo $third;
   ?>
+  <br/>
+  Lowercase : <?php echo strtolower($third); ?><br>
+  Upercase : <?php echo strtoupper($third); ?><br>
+  Upercase first : <?php echo ucfirst($third); ?><br>
+  Upercase words : <?php echo ucwords($third); ?><br>
+  
 </body>
 
 </html>
